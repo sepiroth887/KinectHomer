94c4ef2374a9e9d92b3813b6bc7a898c193ce5da
Tobias Haag <tha@cs.stir.ac.uk>
2012-02-21 16:13:37 +0000

    - forgot to add the Gesture.cs file to git

---
+++ KinectCOM/COMTest/Gesture.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace KinectCOM
+{
+    public class Gesture
+    {
+        private String name;
+        private String context;
+        private ArrayList sequence;
+        
+        public Gesture(String name, String context) {
+            this.name = name;
+            this.context = context;
+        }
+
+        public Gesture(String name, String context,ArrayList seq)
+        {
+            this.name = name;
+            this.context = context;
+            this.sequence = (ArrayList) seq.Clone();
+        }
+
+        public String Name {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public String Context
+        {
+            get { return context; }
+            set { context = value; }
+        }
+
+        public ArrayList Sequence {
+            get { return sequence; }
+            set { sequence = (ArrayList)value.Clone(); }
+        }
+    }
+}
